b2a3acb2cad9df9bd673604b4b623061df89936c
unknown <jason@nevada-technical.com>
2018-03-28 15:57:08 -0700

    poofing coin added

--- A Maze/Assets/UdacityVR/Scripts/Coin.cs
+++ A Maze/Assets/UdacityVR/Scripts/Coin.cs
@@ -6,10 +6,14 @@ public class Coin : MonoBehaviour
 {
     //Create a reference to the CoinPoofPrefab
 
+    public GameObject coinPoof;
+    public static int coinsAcquired = 0;
+
     public void OnCoinClicked() {
-        // Instantiate the CoinPoof Prefab where this coin is located
-        // Make sure the poof animates vertically
-        // Destroy this coin. Check the Unity documentation on how to use Destroy
+        Instantiate(coinPoof, transform.position, Quaternion.Euler(-90, 0, 0));
+        Destroy(gameObject, 0.5f);
+        coinsAcquired = coinsAcquired + 1;
+        
     }
 
 }
